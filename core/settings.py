ca8b088288c93131b3f158098d10cf5eec9fd87c
Miguel-EMC <eduardomuzo123456@gmail.com>
2023-11-14 14:09:32 -0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,6 @@ environ.Env.read_env()
 ENVIROMENT = env
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['.vercel.app']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -152,3 +151,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
